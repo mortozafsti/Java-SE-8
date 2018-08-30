5f6d0c12d8636e3a2c91a53b3eb7cd42c2856cf8
g m mortoza ahmed <mortozafsti@gmail.com>
2018-08-30 12:13:28 +0600

    java Update

---
+++ Java 8/JavaExercise/src/ChapterTwo/ConstantEx.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChapterTwo;
+
+/**
+ * 
+ * @author User
+ * @since v.1.0.0.0
+ */
+public class ConstantEx {
+    /*
+        This is java Project
+    */
+   static final double PI = 3.14;
+   static final int NUMBER_OF_STUDENT = 12;
+   /**
+    * 
+    * @param args 
+    * Just Display the Variable value
+    */
+    public static void main(String[] args) {
+        
+        System.out.println("PI: "+PI);
+        System.out.println("No Of Student: "+NUMBER_OF_STUDENT);
+    }
+}
