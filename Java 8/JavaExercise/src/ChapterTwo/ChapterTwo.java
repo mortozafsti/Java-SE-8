d95a3302267686f9e515d109940c1afde9a089e9
G M Mortoza Ahmed <mortozafsti@gmail.com>
2018-09-05 15:55:05 +0600

    java update

---
+++ Java 8/JavaExercise/src/ChapterTwo/ChapterTwo.java
@@ -0,0 +1,15 @@
+package ChapterTwo;
+
+/**
+ *
+ * @author Cf-37
+ */
+public class ChapterTwo {
+
+    public static void main(String[] args) {
+        long longValue = 500;
+        int intValue = (int) longValue;
+        System.out.println("LongValue: " + longValue);
+        System.out.println("IntValue: " + intValue);
+    }
+}
