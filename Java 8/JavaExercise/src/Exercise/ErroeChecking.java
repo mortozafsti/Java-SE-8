fa115236421d745a736421d0823bdf9b8118e012
G M Mortoza Ahmed <mortozafsti@gmail.com>
2018-09-09 18:51:29 +0600

    java update

---
+++ Java 8/JavaExercise/src/Exercise/ErroeChecking.java
@@ -0,0 +1,17 @@
+
+package Exercise;
+
+/**
+ *
+ * @author Cf-37
+ */
+public class ErroeChecking {
+    public static void main(String[] args) {
+        int k;
+        int j;
+        int i=j=k=2;
+        int d = (int)(Math.random() * 3);   
+        System.out.println("The Result is: "+i+" "+j+" "+k);
+        System.out.println("The Result of D: "+d);
+    }
+}
