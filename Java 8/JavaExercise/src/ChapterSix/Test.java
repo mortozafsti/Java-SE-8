9a6b03b64ae84ff94a3f7769fa9dcbcfe6d12b6e
g m mortoza ahmed <mortozafsti@gmail.com>
2018-09-13 12:51:49 +0600

    java Update

--- Java 8/JavaExercise/src/ChapterSix/Test.java
+++ Java 8/JavaExercise/src/ChapterSix/Test.java
@@ -1,4 +1,3 @@
-
 package ChapterSix;
 
 /**
@@ -6,12 +5,13 @@ package ChapterSix;
  * @author User
  */
 public class Test {
+
     //It is called from another class
     public static void main(String[] args) {
         MethodEx8 re = new MethodEx8();
-        int rs =re.makeSubstituteFromMethod(5, 7);
-        System.out.println("Result: "+rs);
-        
-      re.makeMultiSingleMethod(10, 2);
+        int rs = re.makeSubstituteFromMethod(5, 7);
+        System.out.println("Result: " + rs);
+
+        re.makeMultiSingleMethod(10, 2);
     }
 }
