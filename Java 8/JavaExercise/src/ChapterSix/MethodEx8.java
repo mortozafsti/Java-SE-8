1b05282fe315a6542da31e4cd449c79fa4297bc6
g m mortoza ahmed <mortozafsti@gmail.com>
2018-09-13 12:50:36 +0600

    java Update

--- Java 8/JavaExercise/src/ChapterSix/MethodEx8.java
+++ Java 8/JavaExercise/src/ChapterSix/MethodEx8.java
@@ -6,10 +6,13 @@ package ChapterSix;
  * @author User
  */
 public class MethodEx8 {
+    //It is separation of concern
     public static void main(String[] args) {
         MethodEx8 re = new MethodEx8();
         int rs =re.makeSubstituteFromMethod(5, 7);
         System.out.println("Result: "+rs);
+        
+      re.makeMultiSingleMethod(10, 2);
     }
     
     public int makeSum(int num1, int num2) {
@@ -24,5 +27,9 @@ public class MethodEx8 {
     public int makeSubstituteFromMethod(int n1,int n2) {
         int rs = makeMulti(n1,n2) -makeSum(n1, n2);
         return rs;
+    }
+    public void makeMultiSingleMethod(int num1,int num2){ 
+        int rs = (num1*num2)-(num1+num2);
+        System.out.println("The Result: "+rs);
     }
 }
---
+++ Java 8/JavaExercise/src/ChapterSix/Test.java
@@ -0,0 +1,17 @@
+
+package ChapterSix;
+
+/**
+ *
+ * @author User
+ */
+public class Test {
+    //It is called from another class
+    public static void main(String[] args) {
+        MethodEx8 re = new MethodEx8();
+        int rs =re.makeSubstituteFromMethod(5, 7);
+        System.out.println("Result: "+rs);
+        
+      re.makeMultiSingleMethod(10, 2);
+    }
+}
