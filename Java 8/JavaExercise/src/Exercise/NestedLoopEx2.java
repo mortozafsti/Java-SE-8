df29cbb0ecadbd17529813978adc452657546ad9
G M Mortoza Ahmed <mortozafsti@gmail.com>
2018-09-14 00:36:12 +0600

    java update

--- Java 8/JavaExercise/src/Exercise/NestedLoopEx2.java
+++ Java 8/JavaExercise/src/Exercise/NestedLoopEx2.java
@@ -7,9 +7,13 @@ package Exercise;
  */
 public class NestedLoopEx2 {
     public static void main(String[] args) {
-        for (int i = 1; i < 7; i++) {
-            for (int j = 1; j < i; j++) {
-                System.out.printf("%3d",j);
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j < i) {
+                    System.out.printf("%3d",j);
+                }else{ 
+                    System.out.println();
+                }
             }
             System.out.println();
         }
