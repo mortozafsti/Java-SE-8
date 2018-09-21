969636fc8ae6ad42006d4bdb639f471f00f52ab2
g m mortoza ahmed <mortozafsti@gmail.com>
2018-09-21 10:30:29 +0600

    java Update

---
+++ Java 8/JavaExercise/src/ChapterTen/BoxUnbox.java
@@ -0,0 +1,14 @@
+
+package ChapterTen;
+
+public class BoxUnbox {
+    public static void main(String[] args) {
+        Integer i = new Integer(10);
+        
+        int j = i;
+        
+        
+        System.out.println(""+i);
+        System.out.println(""+j);
+    }
+}
