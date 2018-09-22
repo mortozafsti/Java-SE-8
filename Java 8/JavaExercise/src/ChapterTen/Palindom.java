9e1a9fd36d30f141d7428a7a02b2dd25ca8059f4
g m mortoza ahmed <mortozafsti@gmail.com>
2018-09-22 09:09:19 +0600

    java Update

--- Java 8/JavaExercise/src/ChapterTen/Palindom.java
+++ Java 8/JavaExercise/src/ChapterTen/Palindom.java
@@ -4,14 +4,15 @@ package ChapterTen;
 public class Palindom {
     public static void main(String[] args) {
       
-        
+        System.out.println(isPalindom("abc"));  
     }
     
-    public boolean isPalindom(String s){ 
+    
+    public static boolean isPalindom(String s){ 
         StringBuilder str = new StringBuilder(s);
         boolean status = false;
         String newstr = String.valueOf(str.reverse());
-        if (newstr == s) {
+        if (newstr.equalsIgnoreCase(s)) {
             return true;
         }
         return status;
