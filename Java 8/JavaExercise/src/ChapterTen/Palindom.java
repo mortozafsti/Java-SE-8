400d629d00879b984def5375273a1ca696977d71
g m mortoza ahmed <mortozafsti@gmail.com>
2018-09-22 09:17:08 +0600

    java Update

--- Java 8/JavaExercise/src/ChapterTen/Palindom.java
+++ Java 8/JavaExercise/src/ChapterTen/Palindom.java
@@ -2,13 +2,17 @@
 package ChapterTen;
 
 public class Palindom {
+
     public static void main(String[] args) {
-      
-        System.out.println(isPalindom("abc"));  
+
+        //System.out.println(isPalindom("madam"));
+        
+        String[] srrs = {"mom","abc","madam","wow","mim","eye","bye"};
+        System.out.println(countPaliandom(srrs));
+        
     }
-    
-    
-    public static boolean isPalindom(String s){ 
+
+    public static boolean isPalindom(String s) {
         StringBuilder str = new StringBuilder(s);
         boolean status = false;
         String newstr = String.valueOf(str.reverse());
@@ -16,5 +20,15 @@ public class Palindom {
             return true;
         }
         return status;
+    }
+    
+    public static int countPaliandom(String[] s){ 
+        int counter = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (isPalindom(s[i])) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
