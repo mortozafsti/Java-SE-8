fc388ed0cac357aa037e76a8a53f0eb789b7d854
g m mortoza ahmed <mortozafsti@gmail.com>
2018-09-23 12:59:46 +0600

    java Update

---
+++ Java 8/JavaExercise/src/Exercise/PrimeNum.java
@@ -0,0 +1,16 @@
+
+package Exercise;
+
+import java.math.BigInteger;
+
+public class PrimeNum {
+    public static void main(String[] args) {
+        int x[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    }
+    
+    static void Primes(int[] number){ 
+        for (int i : number) {
+            
+        }
+    }
+}
--- Java 8/JavaExercise/src/Exercise/SumUntilZero.java
+++ Java 8/JavaExercise/src/Exercise/SumUntilZero.java
@@ -6,18 +6,27 @@ public class SumUntilZero {
 
     public static void main(String[] args) {
 
+        //int sum = 0;
         Scanner sc = new Scanner(System.in);
-        
         System.out.println("Enter the Number: ");
-        
+
         int m = 0;
         for (int i = 0;; i++) {
             int a = sc.nextInt();
-            if (a > 0) {
+            if (a != 5) {
                 m +=a;
             }else{ 
                 System.out.println("The Sum Of: "+m);
             }
         }
+        /*
+        do {            
+            System.out.println("Enter the Number: ");
+            m =sc.nextInt();
+            sum +=n;
+        } while (!str == "-1");
+        
+        
+         */
     }
 }
