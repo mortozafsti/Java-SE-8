0d50bf9edd7e1214dce3f9b42f8f93bf55ebd83f
g m mortoza ahmed <mortozafsti@gmail.com>
2018-09-25 11:52:19 +0600

    java Update

---
+++ Java 8/JavaExercise/src/ChapterSeventeen/NewClass.java
@@ -0,0 +1,25 @@
+package ChapterSeventeen;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class NewClass {
+
+    public static void main(String[] args) throws IOException {
+        try (
+                FileOutputStream fileOut = new FileOutputStream("mortoza.txt");) {
+            for (int i = 1; i <= 10; i++) {
+                fileOut.write(i);
+                System.out.println(i);
+            }
+        }
+        try (
+                FileInputStream Out = new FileInputStream("mortoza.txt");) {
+            int value;
+            while ((value = Out.read()) != 1) {
+                System.out.println(value + " ");
+            }
+        }
+    }
+}
