d31fc75a164e3919a29f5e80edb84daba1e5eb78
G M Mortoza Ahmed <mortozafsti@gmail.com>
2018-09-26 08:00:15 +0600

    java update

---
+++ Java 8/JavaExercise/src/Exercise/DecimalValue.java
@@ -0,0 +1,10 @@
+
+package Exercise;
+
+public class DecimalValue {
+    public static void main(String[] args) {
+        int i = 0xFFFFFFF1;
+        int j = ~i;
+        System.out.println(j);
+    }
+}
