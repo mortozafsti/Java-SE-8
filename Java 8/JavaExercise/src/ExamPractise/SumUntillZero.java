c16b92eee67eabeb68d2c1a20f381b5da603fcc4
g m mortoza ahmed <mortozafsti@gmail.com>
2018-09-29 12:52:33 +0600

    java Update

--- Java 8/JavaExercise/src/ExamPractise/SumUntillZero.java
+++ Java 8/JavaExercise/src/ExamPractise/SumUntillZero.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class SumUntillZero {
 
     public static void main(String[] args) {
+        
         int sum = 0;
+        
         Scanner sc = new Scanner(System.in);
+        
         int n = 0;
         
         System.out.println("Please enter a num: ");
         do {
             n = sc.nextInt();
+            
             sum += n;
+            
         } while (n != 0);
         sum += n;
         
