36535a9b49ec21aa4680369d57160a0910d07315
g m mortoza ahmed <mortozafsti@gmail.com>
2018-10-07 09:19:15 +0600

    java Update

---
+++ Java 8/JavaExercise/src/ChapterSeventeen/FileOutputStr.java
@@ -0,0 +1,21 @@
+package ChapterSeventeen;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStr {
+
+   
+    public static void main(String[] args) throws IOException {
+        
+        try (
+                FileOutputStream output = new FileOutputStream("kaka.txt")) {
+            for (int i = 1; i <= 10; i++) {
+                output.write(i);                
+                //System.out.println(i);
+            }
+        }        
+    }
+}
