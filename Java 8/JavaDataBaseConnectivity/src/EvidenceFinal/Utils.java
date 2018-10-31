24ad4d4d95747e9628a63461e2e40ce3c866347f
G M Mortoza Ahmed <mortozafsti@gmail.com>
2018-10-31 23:21:52 +0600

    java update

--- Java 8/JavaDataBaseConnectivity/src/EvidenceFinal/Utils.java
+++ Java 8/JavaDataBaseConnectivity/src/EvidenceFinal/Utils.java
@@ -5,11 +5,15 @@
  */
 package EvidenceFinal;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -36,4 +40,18 @@ public class Utils {
             System.out.println("Could not Log");
         }
     }
+    public static void displayFromFile(String filename,DefaultTableModel model) throws Exception{ 
+        String line;
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(filename+".txt"));
+            while ((line=reader.readLine()) != null) {
+               model.addRow(line.split(", "));
+            }
+            reader.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Buffered Reader Issued");
+        }
+    }
 }
+
