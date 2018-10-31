f20c1fe0ded463f8fc7c1b367c4245c3d60095dc
G M Mortoza Ahmed <mortozafsti@gmail.com>
2018-10-31 21:36:47 +0600

    java update

--- Java 8/JavaDataBaseConnectivity/src/EvidenceFinal/Utils.java
+++ Java 8/JavaDataBaseConnectivity/src/EvidenceFinal/Utils.java
@@ -5,10 +5,35 @@
  */
 package EvidenceFinal;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 /**
  *
  * @author Cf-37
  */
 public class Utils {
-    
+    public static void main(String[] args) {
+        
+    }
+    public static void writeToFile(String filename,List<Student> students) throws Exception{ 
+        File destfile = new File(filename+".txt");
+        try {
+            if (destfile.exists() == false ) {
+            System.out.println("We had to create New file");
+            destfile.createNewFile();
+        }
+            PrintWriter out = new PrintWriter(new FileWriter(destfile,true));
+            for (Student s : students) {
+                out.append(s.getName()+", "+s.getEmail()+", "+s.getAge()+", "+s.getGender()+", "+s.getHobby()+", "+s.getRound()+", "+s.getNote()+"\n");
+               
+            }
+             out.close();
+        } catch (IOException e) {
+            System.out.println("Could not Log");
+        }
+    }
 }
