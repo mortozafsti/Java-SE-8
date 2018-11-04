dc6369e0513081399f613d1446b3b9f45c2e3cb6
g m mortoza ahmed <mortozafsti@gmail.com>
2018-11-04 09:29:03 +0600

    java Update

--- Java 8/JavaDataBaseConnectivity/src/EvidenceSwingFinal/Student.java
+++ Java 8/JavaDataBaseConnectivity/src/EvidenceSwingFinal/Student.java
@@ -8,19 +8,20 @@ public class Student {
     private String age;
     private String gender;
     private String hobby;
+    private String round;
     private String note;
 
     public Student() {
     }
 
-    
-    public Student(String id, String name, String email, String age, String gender, String hobby, String note) {
+    public Student(String id, String name, String email, String age, String gender, String hobby, String round, String note) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.age = age;
         this.gender = gender;
         this.hobby = hobby;
+        this.round = round;
         this.note = note;
     }
 
@@ -48,9 +49,16 @@ public class Student {
         return hobby;
     }
 
+    public String getRound() {
+        return round;
+    }
+
     public String getNote() {
         return note;
     }
+
+    
+    
     
     
 }
--- Java 8/JavaDataBaseConnectivity/src/EvidenceSwingFinal/Utils.java
+++ Java 8/JavaDataBaseConnectivity/src/EvidenceSwingFinal/Utils.java
@@ -1,9 +1,13 @@
 package EvidenceSwingFinal;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 public class Utils {
 
@@ -11,7 +15,7 @@ public class Utils {
 
     }
 
-    public void writeToFile(String filename,List<Utils> students) {
+    public void writeToFile(String filename,List<Student> students) {
         File destfile = new File(filename+".txt");
         try {
             if (destfile.exists() == false) {
@@ -19,14 +23,27 @@ public class Utils {
                 destfile.createNewFile();               
             }
             PrintWriter out = new PrintWriter(new FileWriter(destfile, true));
-            for (Utils s : students) {
-                out.append(s.getn)
+            for (Student s : students) {
+                out.append(s.getId()+", "+s.getName()+", "+s.getEmail()+", "+s.getAge()+", "+s.getGender()+", "+s.getHobby()+", "+s.getRound()+", "+s.getNote());
             }
+            out.close();
         } catch (Exception e) {
+            System.out.println("Could Not Log");
         }
     }
 
-    public void displayFromFile() {
-        
+    public void displayFromFile(String filename,DefaultTableModel model) {
+        String line;
+        BufferedReader reader;
+        try {
+            reader = new  BufferedReader(new FileReader(filename+".txt"));
+            while ((line=reader.readLine())  != null) {
+                model.addRow(line.split(", "));
+                 
+            }
+            reader.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Buffered Reader Issed");
+        }
     }
 }
