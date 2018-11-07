ec2ffac69ad9fee48eea63a7e901f8169f9c4dcf
g m mortoza ahmed <mortozafsti@gmail.com>
2018-11-07 09:01:15 +0600

    java Update

--- Java 8/JavaDataBaseConnectivity/src/EvidenceEight/StudentsView.java
+++ Java 8/JavaDataBaseConnectivity/src/EvidenceEight/StudentsView.java
@@ -366,13 +366,14 @@ public class StudentsView extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       
-        String column[] = {"Id","Pass","Name","Email","Age","Gender","Hobby","Round","Msg"};
-        DefaultTableModel tableModel;
-        tableModel = new DefaultTableModel(0,9);
-        tableModel.setColumnIdentifiers(column);
-        tblDisplay.setModel(tableModel);
-        StudentsUtil.displayFromFile("lll", tableModel); 
+      
+      
+//        String column[] = {"Id","Pass","Name","Email","Age","Gender","Hobby","Round","Msg"};
+//        DefaultTableModel tableModel;
+//        tableModel = new DefaultTableModel(0,9);
+//        tableModel.setColumnIdentifiers(column);
+//        tblDisplay.setModel(tableModel);
+//        StudentsUtil.displayFromFile("lll", tableModel); 
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
